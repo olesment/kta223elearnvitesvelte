346f78194424a4bf1bfb72ff4546d3e683afe8a6
Oskar Aleksander Lesment <oskar-aleksander.lesment@tptlive.ee>
2023-03-03 15:25:32 +0200

    "bulma ja svelt exampl"

---
+++ src/App.svelte
@@ -0,0 +1,10 @@
+<!-- // sarnaselt vuele jagunevad svelt failid 2 v6i 3 tykiks -->
+
+<script>
+
+
+</script>
+
+<!-- Siia tuleb html, ja htmli saab sveti ise2rasusi panna -->
+
+<button class = "button is-primary">Click me</button>
