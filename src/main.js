f0f8e3468be40bd418ab68b752915662304aa179
Oskar Aleksander Lesment <oskar-aleksander.lesment@tptlive.ee>
2023-03-03 14:56:43 +0200

    veel mingi asi

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
-import './app.css'
-import App from './App.svelte'
+import './style.scss'
+// // import App from './App.svelte'
 
-const app = new App({
-  target: document.getElementById('app'),
-})
+// // const app = new App({
+// //   target: document.getElementById('app'),
+// // })
 
-export default app
+// // export default app
+
+// // import hello from './hello.js';
+// hello.hello("esmodule - sissen[utava koodijupi nimetus es impordiks saab ka nimetama");
+
+// // let hello2 = required('./hello2.js'); // vite seda ei toeyta 
+// // hello2.hello('commonjs');
